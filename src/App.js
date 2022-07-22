ac64c143090d891875d97eb21aac22ee51972682
HansKim <khs2016@qroad.net>
2022-07-22 09:24:58 +0900

    CloneCoding-4-component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
+function Food({ name, picture, rating }) 
+{
+  // let { fav } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} width='100%' />
     </div>
   );
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: 'Kimchi',
+    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: 'Samgyeopsal',
+    image: 'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s4000/DSC07624.jpg',
+    rating: 4.9,
+  },
+  {
+    id: 3,
+    name: 'Bibimbap',
+    image: 'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb',
+    rating: 4.8,
+  },
+];
+
+// function renderFood(dish, idx) {
+//   return <Food key={idx} fav={dish.name} picture={dish.image}/>;
+// }
+
+function App() 
+{
+  return (
+    <div>
+      <h1>Hello</h1>
+      {foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>))};
+    </div>
+  );
+}
+// {foodILike.map(renderFood)}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
 }
 
 export default App;
